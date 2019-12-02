1261aff002dba383095e39e0de46cdda3a4afe5b
Becky <houbenyu@gmail.com>
2019-12-02 12:00:24 +0800

    详情页导航栏的封装

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="detail">
+      <detail-nav-bar />
+  </div>
+</template>
+
+<script>
+import DetailNavBar from  './childComps/DetailNavBar'
+export default {
+    components:{
+       DetailNavBar
+    },
+    data(){
+        return{
+            iid:null
+        }
+    },
+    created(){
+        console.log(this.$route.params.iid)
+        this.iid=this.$route.params.iid
+    }
+}
+</script>
+
+<style>
+
+</style>
