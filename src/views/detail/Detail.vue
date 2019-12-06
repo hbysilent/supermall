c16591a0e1b5c430b946025a176d07147482a06c
Becky <houbenyu@gmail.com>
2019-12-06 16:30:45 +0800

    商品评论信息展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,41 +1,106 @@
 <template>
   <div id="detail">
-      <detail-nav-bar />
-      <detail-swiper :top-images="topImages" />
+    <detail-nav-bar class="detail-nav"/>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"/>
+      <detail-base-info :goods="goods"/>
+      <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
+      <detail-param-info :param-info="paramInfo"/>
+      <detail-comment-info :comment-info="commentInfo"/>
+    </scroll>
   </div>
 </template>
 
 <script>
-import DetailNavBar from  './childComps/DetailNavBar'
-import DetailSwiper from './childComps/DetailSwiper'
+  import DetailNavBar from './childComps/DetailNavBar'
+  import DetailSwiper from './childComps/DetailSwiper'
+  import DetailBaseInfo from './childComps/DetailBaseInfo'
+  import DetailShopInfo from './childComps/DetailShopInfo'
+  import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+  import DetailParamInfo from './childComps/DetailParamInfo'
+  import DetailCommentInfo from './childComps/DetailCommentInfo'
 
-import {getDetail} from 'network/detail'
+  import Scroll from 'components/common/scroll/Scroll'
 
-export default {
-    name:'Detail',
-    components:{
-       DetailNavBar,
-       DetailSwiper
+  import {getDetail, Goods, Shop, GoodsParam} from "network/detail";
+
+  export default {
+    name: "Detail",
+    components: {
+      DetailNavBar,
+      DetailSwiper,
+      DetailBaseInfo,
+      DetailShopInfo,
+      DetailGoodsInfo,
+      DetailParamInfo,
+      DetailCommentInfo,
+      Scroll
     },
-    data(){
-        return{
-            iid:null,
-            topImages:[],//轮播图数据
+    data() {
+      return {
+        iid: null,
+        topImages: [],
+        goods: {},
+        shop: {},
+        detailInfo: {},
+        paramInfo: {},
+        commentInfo:{}
+      }
+    },
+    created() {
+      // 1.保存传入的iid
+      this.iid = this.$route.params.iid
+
+      // 2.根据iid请求详情数据
+      getDetail(this.iid).then(res => {
+        // console.log(res);
+        const data = res.result;
+
+        // 1.获取顶部的图片轮播数据
+        this.topImages = data.itemInfo.topImages
+
+        // 2.获取商品信息
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+        // 3.创建店铺信息的对象
+        this.shop = new Shop(data.shopInfo)
+
+        // 4.保存商品的详情数据
+        this.detailInfo = data.detailInfo;
+
+        // 5.获取参数的信息
+        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+
+        //6.取出评论信息
+        if(data.rate.cRate!==0){
+          this.commentInfo=data.rate.list[0]
         }
+      })
     },
-    created(){
-        //1.保存传入的商品id
-        this.iid=this.$route.params.iid
-        //2.根据iid请求详情数据
-        getDetail(this.iid).then(resp=>{
-            console.log(resp)
-            //1)获取轮播图数据
-            this.topImages=resp.result.itemInfo.topImages
-        })
+    methods: {
+      imageLoad() {
+        this.$refs.scroll.refresh()
+      }
     }
-}
+  }
 </script>
 
-<style>
+<style scoped>
+  #detail {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+    height: 100vh;
+  }
 
+  .detail-nav {
+    position: relative;
+    z-index: 9;
+    background-color: #fff;
+  }
+
+  .content {
+    height: calc(100% - 44px);
+  }
 </style>
--- src/views/detail/childComps/DetailNavBar.vue
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -2,57 +2,60 @@
   <div>
     <nav-bar>
       <div slot="left" class="back" @click="backClick">
-          <img src="~assets/img/common/back.svg" alt="">
+        <img src="~assets/img/common/back.svg" alt="">
       </div>
       <div slot="center" class="title">
-          <div 
-          v-for="(item,index) in titles" 
-          class="title-item"
-          :class="{avtive:index===currentIndex}"
-          @click="titleClick(index)"
-          >{{item}}</div>
+        <div v-for="(item, index) in titles"
+             class="title-item"
+             :class="{active: index === currentIndex}"
+             @click="titleClick(index)">
+          {{item}}
+        </div>
       </div>
     </nav-bar>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
+  import NavBar from 'components/common/navbar/NavBar'
 
-export default {
-    components:{
-        NavBar
+  export default {
+    name: "DetailNavBar",
+    components: {
+      NavBar
     },
-    data(){
-        return{
-            titles:['商品','参数','评论','推荐'],
-            currentIndex:0
-        }
+    data() {
+      return {
+        titles: ['商品', '参数', '评论', '推荐'],
+        currentIndex: 0
+      }
     },
-    methods:{
-        titleClick(index){
-            this.currentIndex=index
-        },
-        backClick(){
-            this.$router.go(-1)
-        }
+    methods: {
+      titleClick(index) {
+        this.currentIndex = index
+      },
+      backClick() {
+        this.$router.back()
+      }
     }
-}
+  }
 </script>
 
 <style scoped>
-    .title{
-        display: flex;
-        font-size: 13px;
-    }
-    .title-item{
-        flex: 1;
-        
-    }
-    .avtive{
-        color: var(--color-high-text);
-    }
-    .back{
-        margin-top: 8px;
-    }
+  .title {
+    display: flex;
+    font-size: 13px;
+  }
+
+  .title-item {
+    flex: 1;
+  }
+
+  .active {
+    color: var(--color-high-text)
+  }
+
+  .back img {
+    margin-top: 12px;
+  }
 </style>
